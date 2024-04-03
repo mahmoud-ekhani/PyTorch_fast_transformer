a6149fa59c535af05c94e71c0df0bc6b3deac7cd
Mahmoud Ebrahimkhani <mahmoudebrahimkhani@1910-macbook-30.local>
2024-04-03 15:33:05 -0400

    Added training function

--- train.py
+++ train.py
@@ -273,3 +273,89 @@ def calculate_and_log_metrics(writer, global_step, predicted, expected):
     writer.add_scalar('validation BLEU', bleu, global_step)
     writer.flush()
     
+def train_model(config):
+    # Define the device
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+    if (device == "cuda"):
+        print(f"Device name: {torch.cuda.get_device_name(device.index)}")
+        print(f"Device memory: {torch.cuda.get_device_properties(device.index) / 1024 ** 3} GB")
+    elif (device == "mps"):
+        print(f"Device name: <mps>")
+    else:
+        print("Note: No GPU was found.")
+    device = torch.device(device)
+
+    # Confirm the weights folder exists
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    # Tensorboard
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    # Load a user pre-specified (if any) mdel
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f"Preloading model {model_filename}")
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch.')
+        initial_epoch = 0
+        global_step = 0
+    
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Running epoch {epoch:02d}")
+        for batch in batch_iterator:
+
+            encoder_input = batch["encoder_input"].to(device)
+            decoder_input = batch["decoder_input"].to(device)
+            encoder_mask = batch["encoder_mask"].to(device)
+            decoder_mask = batch["decoder_mask"].to(device)
+            label = batch['label'].to(device)
+
+            encoder_output = model.encode(encoder_input, encoder_mask) # Shape: [batch_size, seq_len, embed_dim]
+            decoder_output = model.decode(encoder_input, encoder_mask, decoder_input, decoder_mask) # Shape: [batch_size, seq_len, embed_dim]
+            proj_output = model.project(decoder_output) # Shape: [batch_size, seq_len, vocab_size]
+
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            writer.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step += 1
+
+            run_validate(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+
+            # Save the model at the end of every epoch
+            model_filename = get_weights_file_path(config, f"{epoch:02d}")
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'global_step': global_step
+            }, model_filename)
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
